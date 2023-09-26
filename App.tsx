197e913c02647c4d558345b38bbe2935382af81e
luizfernando <luiz-fsilva37@hotmail.com>
2023-09-25 22:46:08 -0300

    Organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,33 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
-      <Text
-        style={styles.eventDate}>
-        Segunda, 25 de setembro de 2023
-      </Text>
-    </View>
+    <Home />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-})
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text
+        style={styles.eventDate}>
+        Segunda, 25 de setembro de 2023
+      </Text>
+    </View>
+  )
+}
